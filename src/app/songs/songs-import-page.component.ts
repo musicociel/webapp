a30b80dbb200bcc638bf7f81ad9db4563f027d6d
DivDE <divde@musicociel.fr>
2017-05-24 00:02:42 +0200

    Improving the way files can be imported.

--- src/app/songs/songs-import-page.component.ts
+++ src/app/songs/songs-import-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { autoDetectImporter } from '@musicociel/song-formats/src/song/formats/import';
+import { AlertController } from 'ionic-angular';
+import { importers, importFile } from '@musicociel/song-formats/src/song/formats/import';
 import { readFileAsText } from '../../utils/read-file';
 import { AsyncExecService } from '../async-exec.service';
 import { LibrarySelectorService } from '../libraries/library-selector.service';
@@ -14,35 +15,52 @@ import { LibrarySelectorService } from '../libraries/library-selector.service';
   </ion-header>
   <ion-content>
     <div padding>
-      Musicociel can import songs from various file formats.
+      <ng-container i18n>On this page, you can import songs from files on your device.</ng-container>
       <br><br>
       <input type="file" (change)="importFiles($event.target)" multiple>
     </div>
+    <ion-list>
+      <ion-item>
+        <ion-label i18n>File format</ion-label>
+        <ion-select [(ngModel)]="formatName">
+          <ion-option *ngFor="let importer of importers" [value]="importer.name">{{importer.name}}</ion-option>
+        </ion-select>
+      </ion-item>
+    </ion-list>
   </ion-content>
-  `,
-  styles: []
+  `
 })
 export class SongsImportPageComponent {
+  formatName = 'Auto';
+  importers = importers.list;
 
-  constructor(private asyncExec: AsyncExecService, private librarySelector: LibrarySelectorService) {}
+  constructor(private asyncExec: AsyncExecService, private librarySelector: LibrarySelectorService, private alertCtrl: AlertController) {}
 
   async importFiles(input) {
     const library = await this.librarySelector.selectWritableLibrary();
     if (library) {
+      const errors = [];
       await this.asyncExec.asyncExec(async () => {
         const files = input.files;
         for (const file of files) {
           try {
             const fileContent = await readFileAsText(file);
-            const song = autoDetectImporter.importFile(fileContent);
+            const song = importFile(fileContent, file.name, this.formatName);
             await library.songsManager.add(song);
           } catch (e) {
-            // TODO: better process errors
-            console.log('Error in openFiles: ', e);
+            errors.push(`${file.name}:\n${e}`);
           }
         }
         input.value = null;
       });
+      if (errors.length > 0) {
+        const alertCtrl = this.alertCtrl.create({
+          title: 'Error',
+          message: errors.join('\n'),
+          buttons: ['OK']
+        });
+        alertCtrl.present();
+      }
     }
   }
 
